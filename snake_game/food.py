5526dfe48b074fae0e799db06ecd126554814a15
krzysztofputo <93211139+krzysztofputo@users.noreply.github.com>
2022-05-24 13:51:49 +0200

    Add files via upload

---
+++ snake_game/food.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+from random import randint
+
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("blue")
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        x = randint(-200, 200)
+        y = randint(-200, 200)
+        self.goto(x, y)
