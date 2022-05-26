128c7a557b90f3ee13384139bd09e61a80ba0515
krzysztofputo <93211139+krzysztofputo@users.noreply.github.com>
2022-05-26 13:31:14 +0200

    Add files via upload

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -8,6 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("highscore.txt") as data:
+            self.highscore = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 260)
@@ -15,13 +17,19 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score}  High Score: {self.highscore}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
-        self.clear()
         self.score += 1
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align= ALIGNMENT, font= FONT)
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("highscore.txt", mode='w') as data:
+                data.write(f"{self.highscore}")
+        self.score = 0
+        self.update_scoreboard()
+
+
